90d45f9afb27ee6e268aa1c2fcfda0a0ec461706
Ranjan Sharma <ranjansharma1412@gmail.com>
2026-03-22 21:45:35 +0530

    Updated image into webp

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,6 @@
-const chat_image_bg_dark = require("../images/chat_bg_image.png");
-const profile_placeholder_male = require("../images/profile_male.png");
-const profile_placeholder_female = require("../images/profile_female.png");
+const chat_image_bg_dark = require("../images/chat_bg_image.webp");
+const profile_placeholder_male = require("../images/profile_male.webp");
+const profile_placeholder_female = require("../images/profile_female.webp");
 
 
 export const ImagesAssets = {
